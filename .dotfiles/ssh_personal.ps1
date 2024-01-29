5793952023afd924d12381601bb19f93b69e0fb8
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-01-29 11:45:40 -0300

    Correctly cat the ssh key

--- .dotfiles/ssh_personal.ps1
+++ .dotfiles/ssh_personal.ps1
@@ -3,7 +3,7 @@ echo 'Creating SSH key for the personal account'
 echo ''
 ssh-keygen -t ed25519 -C "13894059+Krauzer94@users.noreply.github.com"
 start msedge https://github.com/settings/keys
-cat .\.ssh\id_ed25519
+cat .\.ssh\id_ed25519.pub
 echo ''
 echo 'New SSH key created under .\.ssh\id_ed25519'
 echo ''
