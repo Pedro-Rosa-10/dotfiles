dc2540277cf5caeb8b86e91d5c2b7cf190ed9dbd
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-01-29 13:20:08 -0300

    Missing script files

---
+++ .dotfiles/winget_install.ps1
@@ -0,0 +1,19 @@
+echo ''
+echo 'Installing applications using winget'
+echo ''
+winget install --id=BleachBit.BleachBit -e  ;
+winget install --id=Discord.Discord -e  ;
+winget install --id=GIMP.GIMP -e  ;
+winget install --id=OBSProject.OBSStudio -e  ;
+winget install --id=Microsoft.PowerToys -e  ;
+winget install --id=Spotify.Spotify -v "1.2.29.605.g66a43ceb" -e  ;
+winget install --id=Microsoft.VisualStudioCode -e  ;
+winget install --id=AmanHarwara.Altus -e  ;
+winget install --id=SomePythonThings.WingetUIStore -e  ;
+winget install --id=VideoLAN.VLC -e  ;
+winget install --id=RARLab.WinRAR -e  ;
+winget install --id=valinet.ExplorerPatcher -e  ;
+winget install --id=Libretro.RetroArch  -e  
+echo ''
+echo 'Finished installing all applications'
+echo ''
