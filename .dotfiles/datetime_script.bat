c17edb691b6276c02d356c29dc471806e7ffbad9
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-04-18 23:35:19 -0300

    Remane some dotfiles

--- .dotfiles/datetime_script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set "dt=%%I"
-set "datestamp=%dt:~6,2%%dt:~4,2%_%dt:~8,2%%dt:~10,2%"
-echo %datestamp% | clip
-echo %datestamp%
