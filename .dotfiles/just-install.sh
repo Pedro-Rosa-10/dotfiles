3f22e668d78beac26d99e87bd78d94d41f85a97b
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-05-27 20:57:16 -0300

    Change just installscript name

--- .dotfiles/just-install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/bin
-curl -LJO https://raw.githubusercontent.com/Krauzer94/home/main/.justfile
