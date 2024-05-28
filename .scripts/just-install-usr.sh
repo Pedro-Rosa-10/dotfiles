a607356c88d1a554b293c0912cb39adbeb53e31e
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-05-28 11:19:28 -0300

    Correct CURL download link for the Justfile

--- .scripts/just-install-usr.sh
+++ .scripts/just-install-usr.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/bin
-curl -LJO https://raw.githubusercontent.com/Krauzer94/home/main/.justfile
+curl -LJO https://raw.githubusercontent.com/Pedro-Rosa-10/home/main/.justfile
