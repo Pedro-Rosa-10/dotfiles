1b575c81d0ab385597c1a1a351eb67dca22c54f0
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-08-13 00:48:42 -0300

    Remove old scripts folder for renaming

--- .config/just-install-usr.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to /usr/bin
-curl -LJO https://raw.githubusercontent.com/Pedro-Rosa-10/home/main/.justfile
