995f497672ecd7fe7a6947531533b3e2e3478669
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-09-04 17:29:56 -0300

    Correct 2 space identation

--- .flake/flake.nix
+++ .flake/flake.nix
@@ -1,83 +1,71 @@
 {
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    outputs = {
-        nixpkgs,
-        home-manager,
-        nixos-wsl,
-        ...
-    }:
-    let
-        system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-        # NixOS on WSL System configuration
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            modules = [
-            # nixos(wsl)@nixos
-            (import "${nixos-wsl}/modules")
-                {
-                    wsl.enable = true;
-                    wsl.defaultUser = "nixos";
-                    # System packages https://search.nixos.org/packages
-                    environment.systemPackages = with pkgs; [
-                        direnv
-                    ];
-                    # Setup VSCode Remote
-                    programs.nix-ld = {
-                        enable = true;
-                        package = pkgs.nix-ld-rs;
-                    };
-                    system.stateVersion = "24.05";
-                }
-            ];
-        };
-        # nixos(wsl)@nixos
-        homeConfigurations."nixos@nixos" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-                {
-                    home.username = "nixos";
-                    home.homeDirectory = "/home/nixos";
-                    home.stateVersion = "23.11";
-                    # System packages https://search.nixos.org/packages
-                    home.packages = with pkgs; [
-                        just
-                        wget
-                        git
-                        fastfetch
-                    ];
-                    programs.home-manager.enable = true;
-                }
-            ];
-        };
-        # debian(wsl)@POAN23090675
-        homeConfigurations."krauzer@POAN23090675" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-            modules = [
-                {
-                    home.username = "krauzer";
-                    home.homeDirectory = "/home/krauzer";
-                    home.stateVersion = "23.11";
-                    # System packages https://search.nixos.org/packages
-                    home.packages = with pkgs; [
-                        just
-						wget
-						git
-						direnv
-						fastfetch
-                    ];
-                    programs.home-manager.enable = true;
-                }
-            ];
-        };
+  };
+  outputs = { nixpkgs, home-manager, nixos-wsl, ... }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in
+  {
+    # nixos@nixos > cofiguration.nix
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        (import "${nixos-wsl}/modules") {
+          wsl.enable = true;
+          wsl.defaultUser = "nixos";
+          environment.systemPackages = with pkgs; [
+            git
+            wget
+          ];
+          # Setup VSCode Remote
+          programs.nix-ld = {
+            enable = true;
+            package = pkgs.nix-ld-rs;
+          };
+          system.stateVersion = "24.05";
+        }
+      ];
     };
+    # nixos@nixos > home.nix
+    homeConfigurations."nixos@nixos" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        {
+          home.username = "nixos";
+          home.homeDirectory = "/home/nixos";
+          home.stateVersion = "23.11";
+          home.packages = with pkgs; [
+            just
+            direnv
+            fastfetch
+          ];
+          programs.home-manager.enable = true;
+        }
+      ];
+    };
+    # krauzer@POAN23090675 > home.nix
+    homeConfigurations."krauzer@POAN23090675" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        {
+          home.username = "krauzer";
+          home.homeDirectory = "/home/krauzer";
+          home.stateVersion = "23.11";
+          home.packages = with pkgs; [
+            just
+            direnv
+            fastfetch
+          ];
+          programs.home-manager.enable = true;
+        }
+      ];
+    };
+  };
 }
