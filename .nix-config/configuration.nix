8c49fbda9d8d9a9f76f1060db1dadb5356dfa717
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-09-04 00:24:06 -0300

    NixOS WSL configuration file

---
+++ .nix-config/configuration.nix
@@ -0,0 +1,36 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+# NixOS-WSL specific options are documented on the NixOS-WSL repository:
+# https://github.com/nix-community/NixOS-WSL
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports = [
+    # include NixOS-WSL modules
+    <nixos-wsl/modules>
+  ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "nixos";
+
+  # System packages
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    direnv
+    fastfetch
+  ];
+
+  # Setup VSCode Remote
+  programs.nix-ld = {
+      enable = true;
+      package = pkgs.nix-ld-rs;
+  };
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
