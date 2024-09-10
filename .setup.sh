f6890d9e37b2e575d9757503dd9ae3e775e67f58
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-09-10 11:15:43 -0300

    Revert sutil changes

--- .setup.sh
+++ .setup.sh
@@ -8,16 +8,18 @@ setup_dotfiles() {
     mv ~/dotfiles/.git ~/
     rm -rdf ~/dotfiles
     git restore .
+    . ~/.bashrc
 }
 
 # Install Just
 install_just () {
     mkdir -p ~/.local/bin
     curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
-    export PATH=$PATH:~/.local/bin
+    export PATH=$PATH:~/.local/bin  # Ensure just is in PATH
     . ~/.bashrc
 }
 
 setup_dotfiles
 install_just
 just installs-wsl
+echo -e "\n  . ~/.bashrc && just installs-wsl\n"
