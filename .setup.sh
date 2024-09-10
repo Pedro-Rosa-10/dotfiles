c6db6bfbbc823626188e60959b4fbe85b94a2ca4
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-09-10 01:42:39 -0300

    Bootstrap for WSL Distro

---
+++ .setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Set up dotfiles
+setup_dotfiles() {
+    echo '*' > ~/.gitignore
+    mv ~/.bashrc ~/.bashrc.old
+    git clone https://github.com/Pedro-Rosa-10/dotfiles.git
+    mv ~/dotfiles/.git ~/
+    rm -rdf ~/dotfiles
+    git restore .
+}
+
+# Install Just
+install_just () {
+    mkdir -p ~/.local/bin
+    curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
+    source ~/.bashrc
+    just installs-wsl
+}
+
+setup_dotfiles
+install_just
