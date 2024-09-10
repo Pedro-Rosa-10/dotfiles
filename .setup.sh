66bf611668af9f4b348fb7b12c62c826743403c3
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-09-10 11:04:17 -0300

    Ensures just recipe runs

--- .setup.sh
+++ .setup.sh
@@ -8,17 +8,17 @@ setup_dotfiles() {
     mv ~/dotfiles/.git ~/
     rm -rdf ~/dotfiles
     git restore .
-    source ~/.bashrc
+    . ~/.bashrc
 }
 
 # Install Just
 install_just () {
     mkdir -p ~/.local/bin
     curl --proto '=https' --tlsv1.2 -sSf https://just.systems/install.sh | bash -s -- --to ~/.local/bin
-    source ~/.bashrc
+    # export PATH=$PATH:~/.local/bin
+    . ~/.bashrc
 }
 
 setup_dotfiles
 install_just
 just installs-wsl
-echo -e "\n  source ~/.bashrc && just installs-wsl\n"
