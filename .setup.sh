fa12f2f23dc4972545e6d5ee5ba439f6ad749223
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2024-09-11 23:29:01 -0300

    Bootstrap Fedora and also Debian/Ubuntu on WSL

--- .setup.sh
+++ .setup.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+# Install Git
+install_git() {
+    if [[ -f /etc/os-release ]]; then
+        . /etc/os-release
+        DISTRO=$ID
+    else
+        echo "Cannot determine the distribution, /etc/os-release not found"
+        return 1
+    fi
+
+    case "$DISTRO" in
+        fedora)
+            sudo dnf install git -y
+            ;;
+        ubuntu | debian)
+            sudo apt install git -y
+            ;;
+        *)
+            echo "Unknown distro, nothing to do here"
+            ;;
+    esac
+}
+
 # Set up dotfiles
 setup_dotfiles() {
     echo '*' > ~/.gitignore
@@ -17,6 +40,7 @@ install_just () {
     export PATH=$PATH:~/.local/bin
 }
 
+# Execute all functions
+install_git
 setup_dotfiles
 install_just
-just installs-wsl
