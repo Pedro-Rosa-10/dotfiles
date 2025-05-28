3c10d7c9bccc7d6f99e37e43945108339d2f9f1d
Pedro-Rosa-10 <144266741+Pedro-Rosa-10@users.noreply.github.com>
2025-05-28 14:11:41 -0300

    Improve dotfiles repo setting up

--- .setup.sh
+++ .setup.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
 # Set up dotfiles
-echo '*' > ~/.gitignore
-mv ~/.bashrc ~/.bashrc.old
 git clone https://github.com/Pedro-Rosa-10/dotfiles.git
-mv ~/dotfiles/.git ~/
-rm -rdf ~/dotfiles
-git restore .
+mv ~/dotfiles/.git ~/dotfiles/.gitignore ~/
+rm -rdf ~/dotfiles && git restore .
 
 # Install Scoop apps
 scoop import ~/.scoop.json
